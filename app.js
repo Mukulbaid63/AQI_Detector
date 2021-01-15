e968159f1c5b24cbc5f47289692d31157f0f08b7
Muddhit Baid <61585314+Mukulbaid63@users.noreply.github.com>
2021-01-15 17:53:11 +0545

    Update app.js

--- app.js
+++ app.js
@@ -1,55 +1,60 @@
+//apikey and url defining
 var temp = {
     key: "87cde31000e5170290cebdd94820d23d",
     base: "https://api.openweathermap.org/data/2.5/air_pollution?"
   }
 var notificationElement = document.querySelector(".notification");
+
+//Accessing the location from the browser using geo location
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(setPosition);
     notificationElement.innerHTML = "";
 
 }
 else {
+    //if your browser doesnt supports geolocation error message will be displayed.
     notificationElement.innerHTML = "Geolocation is not supported by this browser.";
   }
+
+//passes the coordinates to the APi and does a function call
 function setPosition(position){
-    let latitude = position.coords.latitude;
-    let longitude = position.coords.longitude;
-    console.log(latitude);
-    console.log(longitude);    
+    let latitude = position.coords.latitude;//storing latitude
+    let longitude = position.coords.longitude;//storing longitude
+      
 getAQI(latitude, longitude);
   }
 function getAQI(lat,lon) {
-      fetch(`${temp.base}lat=${lat}&lon=${lon}&appid=${temp.key}`)
+      fetch(`${temp.base}lat=${lat}&lon=${lon}&appid=${temp.key}`)//fetching data from api
         .then(aqi => {
           return aqi.json();
-        }).then(displayResults);
+        }).then(displayResults);//function call to display results
 }
 function displayResults(aq){
-    let aqi=document.querySelector(".aqi");
-    aqi.innerHTML=`${aq.list[0].main.aqi}`;
+    let aqi=document.querySelector(".aqi");//selecting the aqi element from HTML
+    aqi.innerHTML=`${aq.list[0].main.aqi}`;//passing the json value in it
 
-    let co=document.querySelector(".co-util");
-    co.innerHTML=`${aq.list[0].components.co}`
+    let co=document.querySelector(".co-util");//selecting the aqi element from HTML
+    co.innerHTML=`${aq.list[0].components.co}`//passing the json value in it
 
-    let no=document.querySelector(".no-util");
-    no.innerHTML=`${aq.list[0].components.no}`
+    let no=document.querySelector(".no-util");//selecting the aqi element from HTML
+    no.innerHTML=`${aq.list[0].components.no}`//passing the json value in it
 
-    let no2=document.querySelector(".no2-util");
-    no2.innerHTML=`${aq.list[0].components.no2}`
+    let no2=document.querySelector(".no2-util");//selecting the aqi element from HTML
+    no2.innerHTML=`${aq.list[0].components.no2}`//passing the json value in it
 
-    let o3=document.querySelector(".o3-util");
-    o3.innerHTML=`${aq.list[0].components.o3}`
+    let o3=document.querySelector(".o3-util");//selecting the aqi element from HTML
+    o3.innerHTML=`${aq.list[0].components.o3}`//passing the json value in it
 
-    let so2=document.querySelector(".so2-util");
-    so2.innerHTML=`${aq.list[0].components.so2}`
+    let so2=document.querySelector(".so2-util");//selecting the aqi element from HTML
+    so2.innerHTML=`${aq.list[0].components.so2}`//passing the json value in it
 
-    let pm2=document.querySelector(".pm2_5-util");
-    pm2.innerHTML=`${aq.list[0].components.pm2_5}`
+    let pm2=document.querySelector(".pm2_5-util");//selecting the aqi element from HTML
+    pm2.innerHTML=`${aq.list[0].components.pm2_5}`//passing the json value in it
 
-    let nh3=document.querySelector(".nh3-util");
-    nh3.innerHTML=`${aq.list[0].components.nh3}`
+    let nh3=document.querySelector(".nh3-util");//selecting the aqi element from HTML
+    nh3.innerHTML=`${aq.list[0].components.nh3}`//passing the json value in it
 
-    let pm10=document.querySelector(".pm10-util");
-    pm10.innerHTML=`${aq.list[0].components.pm10}`
+    let pm10=document.querySelector(".pm10-util");//selecting the aqi element from HTML
+    pm10.innerHTML=`${aq.list[0].components.pm10}`//passing the json value in it
 
 }
