819426bbbd60da70d64ac782edcdf270ff50d439
Muddhit Baid <61585314+Mukulbaid63@users.noreply.github.com>
2021-01-17 14:35:25 +0545

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 //apikey and url defining
+// **1
 var temp = {
     key: "87cde31000e5170290cebdd94820d23d",
     base: "https://api.openweathermap.org/data/2.5/air_pollution?"
   }
+// **1
+
+
 var notificationElement = document.querySelector(".notification");
 
+
+// **2
 //Accessing the location from the browser using geo location
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(setPosition);
@@ -15,7 +21,10 @@ else {
     //if your browser doesnt supports geolocation error message will be displayed.
     notificationElement.innerHTML = "Geolocation is not supported by this browser.";
   }
+// **2
 
+
+// **3
 //passes the coordinates to the APi and does a function call
 function setPosition(position){
     let latitude = position.coords.latitude;//storing latitude
@@ -23,12 +32,21 @@ function setPosition(position){
       
 getAQI(latitude, longitude);
   }
+// **3
+
+
+
+// **4
 function getAQI(lat,lon) {
       fetch(`${temp.base}lat=${lat}&lon=${lon}&appid=${temp.key}`)//fetching data from api
         .then(aqi => {
           return aqi.json();
         }).then(displayResults);//function call to display results
 }
+// **4
+
+
+// **5
 function displayResults(aq){
     let aqi=document.querySelector(".aqi");//selecting the aqi element from HTML
     aqi.innerHTML=`${aq.list[0].main.aqi}`;//passing the json value in it
@@ -58,3 +76,4 @@ function displayResults(aq){
     pm10.innerHTML=`${aq.list[0].components.pm10}`//passing the json value in it
 
 }
+// **5
